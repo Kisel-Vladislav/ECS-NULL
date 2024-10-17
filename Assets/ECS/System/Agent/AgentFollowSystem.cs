94b794cee6be80ed8546f73500609e07a2a2b377
Kisel_Vladislav <d.988.1861.t@gmail.com>
2024-10-18 00:58:56 +0300

    ♻️ Add minimum follow distance for enemies

--- Assets/ECS/System/Agent/AgentFollowSystem.cs
+++ Assets/ECS/System/Agent/AgentFollowSystem.cs
@@ -1,12 +1,14 @@
 ﻿using CodeBase.ECS.Component;
 using CodeBase.ECS.Component.Agent;
 using Leopotam.Ecs;
+using UnityEngine;
 
 namespace CodeBase.ECS.System.Agent
 {
     public class AgentFollowSystem : IEcsRunSystem
     {
-        private EcsFilter<AgentComponent, Follow, AnimatorRef> followingEnemies;
+        private EcsFilter<AgentComponent, Follow, AnimatorRef, TransformRef> followingEnemies;
+        private const float MinFollowDistance = 1.5f;
 
         public void Run()
         {
@@ -14,13 +16,20 @@ namespace CodeBase.ECS.System.Agent
             {
                 ref var entity = ref followingEnemies.GetEntity(i);
                 ref var enemy = ref followingEnemies.Get1(i);
+                ref var transform = ref followingEnemies.Get4(i);
+
                 ref var follow = ref followingEnemies.Get2(i);
 
                 if (follow.Entity.IsAlive())
                 {
                     enemy.navMeshAgent.enabled = true;
                     var targetPos = follow.Target.position;
-                    enemy.navMeshAgent.SetDestination(targetPos);
+                    var distanceToTarget = Vector3.Distance(transform.transform.position, targetPos);
+
+                    if(distanceToTarget > MinFollowDistance)
+                        enemy.navMeshAgent.SetDestination(targetPos);
+                    else
+                        enemy.navMeshAgent.ResetPath();
                 }
                 else
                 {
