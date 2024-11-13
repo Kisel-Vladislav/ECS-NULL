ca029c0db6bf8ce7bac70a34a4ba4fb6708ec684
Kisel_Vladislav <d.988.1861.t@gmail.com>
2024-11-13 07:40:47 +0300

    add grid shader

--- Assets/CodeBase/Grid/LevelGrid.cs
+++ Assets/CodeBase/Grid/LevelGrid.cs
@@ -4,11 +4,22 @@ namespace CodeBase.Grid
 {
     public class LevelGrid : MonoBehaviour
     {
+        private const int X = 100;
+        private const int Z = 100;
+        private int _cellSize = 2;
         private Grid<Build> _grid;
-
+        [SerializeField] Material _gridMaterial;
         private void Awake()
         {
-            _grid = new Grid<Build>(10, 10, 2);
+            _grid = new Grid<Build>(X, Z, _cellSize);
+
+            float planeWidth = X * _cellSize;
+            float planeLength = Z * _cellSize;
+
+            var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            plane.transform.localScale = new Vector3(planeWidth / 10, 1, planeLength / 10);
+            plane.transform.position = Vector3.zero;
+            plane.GetComponent<Renderer>().material = _gridMaterial;
         }
         public Vector3 GetWorldPosition(Vector3 position) =>
             _grid.GetWorldPosition(position);
