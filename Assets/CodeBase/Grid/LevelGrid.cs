e709531eb95645ddd231afaa6b189d461a98fd6d
Kisel_Vladislav <d.988.1861.t@gmail.com>
2024-11-15 20:22:00 +0300

    Add grid levels

--- Assets/CodeBase/Grid/LevelGrid.cs
+++ Assets/CodeBase/Grid/LevelGrid.cs
@@ -1,58 +1,90 @@
-﻿using CodeBase.Infrastructure.StaticData;
+﻿using Assets;
+using CodeBase.Infrastructure.StaticData;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CodeBase.Grid
 {
     public class LevelGrid : MonoBehaviour
     {
-        private const int X = 5;
-        private const int Z = 5;
+        private const int X = 50;
+        private const int Z = 50;
+        private const int GridLevels = 3;
+        private const int CellSize = 2;
+
+        [SerializeField] private Material _gridMaterial;
 
         private class BuildCell
         {
             public GameObject GameObject;
         }
 
-        private int _cellSize = 2;
-        private Grid<BuildCell> _grid;
-        [SerializeField] Material _gridMaterial;
+        private List<Grid<BuildCell>> _levels;
 
         private void Awake()
         {
-            _grid = new Grid<BuildCell>(X, Z, _cellSize);
-
-            float planeWidth = X * _cellSize;
-            float planeLength = Z * _cellSize;
-
-            var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-            plane.transform.localScale = new Vector3(planeWidth / 10, 1, planeLength / 10);
-            plane.transform.position = Vector3.zero;
-            plane.GetComponent<Renderer>().material = _gridMaterial;
+            InitGridLevels();
+            CreateGridPlane();
         }
 
-        public Vector3 GetWorldPosition(Vector3 position) =>
-            _grid.GetWorldPosition(position);
+        public Vector3 GetWorldPosition(Vector3 position)
+        {
+            var grid = GetGridForPosition(position);
+            if (grid == null)
+                return Vector3.zero;
 
+            float levelYOffset = RoundedToLevelIndex(position) * CellSize;
+            return grid.GetWorldPosition(position).AddY(levelYOffset);
+        }
         public void PlaceBuild(Vector3 position, Build build)
         {
-            int x, z;
-            _grid.GetCellIndicesFromPosition(position, out x, out z);
+            var grid = GetGridForPosition(position);
+            if (grid == null)
+                return;
 
-            var cell = _grid.Get(x, z);
-
+            var cell = grid.Get(position);
             if (cell == null || !IsCellEmpty(cell))
                 return;
 
-            var worldPosition = _grid.GetWorldPosition(x, z);
+            var worldPosition = GetWorldPosition(position);
 
-            cell.Value = new BuildCell();
-            cell.Value.GameObject = Instantiate(build.prefab, worldPosition, Quaternion.identity, transform);
+            cell.Value = new BuildCell
+            {
+                GameObject = Instantiate(build.prefab, worldPosition, Quaternion.identity, transform)
+            };
         }
 
-        private bool IsCellEmpty(Grid<BuildCell>.GridTile cell)
+        private void InitGridLevels()
         {
-            return cell.Value == null || cell.Value.GameObject == null;
-
+            _levels = new List<Grid<BuildCell>>();
+            for (int i = 0; i < GridLevels; i++)
+            {
+                var grid = new Grid<BuildCell>(X, Z, CellSize);
+                _levels.Add(grid);
+            }
         }
+        private void CreateGridPlane()
+        {
+            float planeWidth = X * CellSize;
+            float planeLength = Z * CellSize;
+
+            var plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+            plane.transform.localScale = new Vector3(planeWidth / 10, 1, planeLength / 10);
+            plane.transform.position = Vector3.zero;
+
+            plane.GetComponent<Renderer>().material = _gridMaterial;
+        }
+        private Grid<BuildCell> GetGridForPosition(Vector3 position)
+        {
+            int levelIndex = RoundedToLevelIndex(position);
+            return IsLevelIndexValid(levelIndex) ? _levels[levelIndex] : null;
+        }
+        private int RoundedToLevelIndex(Vector3 position) => 
+            Mathf.RoundToInt(position.y / CellSize);
+        private bool IsLevelIndexValid(int levelIndex) => 
+            levelIndex >= 0 && levelIndex < _levels.Count;
+        private bool IsCellEmpty(Grid<BuildCell>.GridTile cell) => 
+            cell.Value == null || cell.Value.GameObject == null;
     }
 }
